c601a4aae9696dea460b72cf9eb7a3605fabb123
Egor Stahovskiy <egor.stahovskiy@mail.ru>
2018-01-15 15:16:26 +0300

    Menu

---
+++ src/utils/menus/Menu.java
@@ -0,0 +1,16 @@
+/*
+*Project TravelAgency
+* @version 1.0
+* @date December2017
+* @author George Stahovsky
+*/
+package utils.menus;
+
+import utils.Constants;
+
+public class Menu {
+    protected static void PrintMenuConstants() {
+        System.out.println(Constants.PREVIOUS_MENU);
+        System.out.print(Constants.CHOSE_COMMAND);
+    }
+}
